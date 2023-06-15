9c810cf40de6403aa86c8aea441e3812fd24cef7
nkasaby <98822809+nkasaby@users.noreply.github.com>
2023-06-15 17:05:26 +0300

    Update Ctrl.java

--- src/Ctrl.java
+++ src/Ctrl.java
@@ -7,15 +7,18 @@ public class Ctrl {
             String userInput = View.userInput();
             if (Puzzle.validInput(userInput)) {
                 byte[] convertedUserInput = Puzzle.convertUserInput(userInput);
-
                 byte [] address16 = Puzzle.numAddress(Puzzle.puzzleBoard, (byte) 16);
 
                 if (Puzzle.validMove(Puzzle.puzzleBoard, convertedUserInput)) {
                     Puzzle.updateBoard(Puzzle.puzzleBoard,  convertedUserInput[0], convertedUserInput[1], address16[0], address16[1]);
                     View.printPuzzleBoard(Puzzle.puzzleBoard, Puzzle.rows, Puzzle.cols);
-                } else View.invalidMoveMessage(Puzzle.validMove(Puzzle.puzzleBoard, convertedUserInput));
+                } else {
+                    View.invalidMoveMessage(Puzzle.validMove(Puzzle.puzzleBoard, convertedUserInput));
+                    View.printPuzzleBoard(Puzzle.puzzleBoard, Puzzle.rows, Puzzle.cols);
+                }
             } else {
                 View.invalidMoveMessage(false);
+                View.printPuzzleBoard(Puzzle.puzzleBoard, Puzzle.rows, Puzzle.cols);
             }
         }
         View.printWinMessage(true);
